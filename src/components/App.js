ca7fd3f1abc7ad9b421219f77ce6dfaaa71d3c50
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-05-02 22:38:46 -0400

    commit tree 5031ccc43f9df3d3e0f773eff2e0667ceac956b1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
 import React, {useState} from "react";
 import Nav from "./Nav";
 import Oinkers from "./Oinkers";
-
+import PigForm from "./PigForm";
 import pigs from "../porkers_data";
 
 function App() {
 	const [hogs, setHogs]= useState(pigs.map(obj=>({...obj, hidden:false})))
 	const [greaseFilterOn, setGreaseFilterOn]= useState(false)
-	
+	const [newHog, setNewHog]= useState({
+		name:"",
+		specialty:"",
+		greased:false,
+		weight:0,
+		"highest medal achieved": "wood",
+		image:"",
+		hidden:false
+	})
+	//name, spec, greased, weight, medal, image
+	function onFormChange(e){
+		const name= e.target.name
+		let value= e.target.value
+		if(name === 'weight'){
+			value=parseFloat(value)
+		}
+		setNewHog({...newHog, [name]:value})
+	}
+	console.log(hogs)
+	function onFormSubmit(e){
+		e.preventDefault()
+		setHogs([...hogs, newHog])
+		setNewHog({
+			name:"",
+			specialty:"",
+			greased:false,
+			weight:0,
+			"highest medal achieved": "wood",
+			image:"",
+			hidden:false
+		})
+	}
 	function onHidePig(pigName){
 		setHogs(prevState=>prevState.map(pig=>pig.name===pigName?{...pig, hidden:true}:pig))
 	}
@@ -22,12 +53,16 @@ function App() {
 	}
 
 	function weightSort(){
-		setHogs(prevState=>prevState.sort((a,b)=> a.weight - b.weight))
+		setHogs(prevState=>[...prevState].sort((a,b)=> a.weight - b.weight))
 	}
 
+	function nameSort(){
+		setHogs(prevState=>[...prevState].sort((a,b)=>(a.name>b.name)?1:((b.name > a.name)?-1:0)))
+	}
 	return (
 		<div className="App">
-			<Nav greaseFilterOn={greaseFilterOn} onGreaseFilter={onGreaseFilter} onUnhide={onUnhide} weightSort={weightSort}/>
+			<Nav greaseFilterOn={greaseFilterOn} onGreaseFilter={onGreaseFilter} onUnhide={onUnhide} weightSort={weightSort} nameSort={nameSort}/>
+			<PigForm pig={newHog} onFormChange={onFormChange} onFormSubmit={onFormSubmit}/>
 			<Oinkers hogs={hogs} greaseFilterOn={greaseFilterOn} onHidePig={onHidePig}/>
 		</div>
 	);
